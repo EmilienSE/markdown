e12f4251b1a95edb1e7773194505dee93533c509
EmilienSE <esechilariu@yahoo.fr>
2019-04-02 11:44:04 +0200

    initial commit

---
+++ routes/index.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var router = express.Router();
+var markdown = require( "markdown-it" )();
+const bodyParser = require('body-parser'); // Charge le middleware de gestion des paramètres
+const urlencodedParser = bodyParser.urlencoded({ extended: false });
+var cookieParser = require('cookie-parser');
+
+router.use(cookieParser());
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  if(req.cookies['text']){
+  	console.log(req.cookies);
+  	var text = req.cookies['text'];
+    res.clearCookie('text', {httpOnly: true});
+    htmlMarkdown = markdown.render(text);
+    console.log(htmlMarkdown);
+    res.render('index', { title: 'Express', htmlMarkdown: htmlMarkdown});
+  } else {
+  	res.render('index', { title: 'Express'});
+  }
+});
+
+router.post('/mdown', urlencodedParser, function(req, res){
+  res.cookie('text', req.body.text, {httpOnly: true});
+  res.redirect('/');
+});
+
+module.exports = router;
